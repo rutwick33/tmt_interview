c46689d096e5e50aae3d39f126d17cc76f1bcbba
Rutwick Bhawsar <rutwickbhawsar@Rutwicks-MacBook-Pro.local>
2023-04-04 23:14:23 +0530

    Challenge 6 - Tags on Order - Completed

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,11 +1,14 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView, DeactivateOrderView, OrderFilterDateView
+from interview.order.views import OrderListCreateView, \
+    OrderTagListCreateView, DeactivateOrderView, \
+        OrderFilterDateView, OrderTagCreateView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
+    path('tags/<int:id>', OrderTagCreateView.as_view(), name='order-list'),
     path('deactivate/<int:id>/', DeactivateOrderView.as_view(), name='deactivate-order'),
     path('date/<str:start_date>&<str:embargo_date>',OrderFilterDateView.as_view(), name = 'date-range-order')
 ]

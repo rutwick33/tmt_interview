6ee5f8111a64e096719084d692a72721cbe8aac1
Rutwick Bhawsar <43450725+rutwick33@users.noreply.github.com>
2023-04-05 23:10:22 +0530

    Revert "Challenge 7 - Orders on Tag - Completed"

--- interview/order/views.py
+++ interview/order/views.py
@@ -3,9 +3,8 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.request import Request
 
-from django.db.models import Prefetch
 from interview.order.models import Order, OrderTag
-from interview.order.serializers import OrderSerializer, OrderTagSerializer, OrderTagOnlySerializer, OrderSpecificTagSerializer
+from interview.order.serializers import OrderSerializer, OrderTagSerializer, OrderTagOnlySerializer
 
 from datetime import datetime
 
@@ -65,21 +64,3 @@ class OrderTagCreateView(generics.ListAPIView):
     def get_queryset(self, **kwargs):
         return self.queryset.get(**kwargs)
     
-class TagOrderCreateView(generics.ListAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSpecificTagSerializer
-
-    def get(self, request: Request, *args, **kwargs) -> Response:
-
-        try:
-            order = Order.objects.exclude(tags__isnull=True).prefetch_related(
-                Prefetch('tags', queryset=OrderTag.objects.filter(id=kwargs['id']), to_attr='filtered_tags')
-            )
-            orderlist = [eachorder for eachorder in order.values()]
-            print(orderlist[0])
-            serializer = self.serializer_class(order,many=True)
-
-            return Response(serializer.data, status=200)
-        
-        except Exception as e:
-            print("********* Exception: ",e)
